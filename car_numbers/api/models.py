51af69d8ea6d60a9d85b7fdc02a3c81913b2da9a
Tozix <tozix@yandex.ru>
2023-01-15 16:47:53 +0700

    flake fix

--- car_numbers/api/models.py
+++ car_numbers/api/models.py
@@ -5,16 +5,20 @@ from django.db import models
 
 
 class CarNumer(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    plate = models.CharField('Гос. номер',
-                             unique=True,
-                             max_length=9,
-                             validators=[
-                                 RegexValidator(
-                                     regex='^[ABEKMHOPCTYX]\d{3}[ABEKMHOPCTYX]{2}$',
-                                     message='Недопустимые символы в номере!'
-                                 )
-                             ])
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    plate = models.CharField(
+        'Гос. номер',
+        unique=True,
+        max_length=9,
+        validators=[
+            RegexValidator(
+                regex=r'^[ABEKMHOPCTYX]\d{3}[ABEKMHOPCTYX]{2}$',
+                message='Недопустимые символы в номере!'
+            )
+        ])
 
     class Meta:
         verbose_name = 'Номер машины'
